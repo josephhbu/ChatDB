4e07ed067ca3731ffe0851d8fe6bf2b762385c31
josephhbu <joseph.hbu@gmail.com>
2024-12-13 15:04:01 -0800

    Update chatDB_main.py

--- chatDB_main.py
+++ chatDB_main.py
@@ -9,7 +9,7 @@ import os
 
 from nlp import process_user_input
 from query_patterns import generator
-from queries_suggestions import process_sample_queries
+from query_suggestions import process_sample_queries
 from backend_functions import implement
 from nosql_backend import import_multiple_json_to_mongodb, csv_to_json
 from mongo_queries import process_user_input_mongodb
